d797ace17b38467d1d19a02468b2694c22c725dd
nickngweipeng <15056201@myrp.edu.sg>
2017-05-18 13:26:52 +0800

    OK

---
+++ app/src/main/java/com/example/nickng/p05_ndpsongs/DBHelper.java
@@ -0,0 +1,8 @@
+package com.example.nickng.p05_ndpsongs;
+
+/**
+ * Created by 15056201 on 18/5/2017.
+ */
+
+public class DBHelper {
+}
---
+++ app/src/main/java/com/example/nickng/p05_ndpsongs/Song.java
@@ -0,0 +1,42 @@
+package com.example.nickng.p05_ndpsongs;
+
+import java.io.Serializable;
+
+/**
+ * Created by 15056201 on 18/5/2017.
+ */
+
+public class Song implements Serializable {
+    private int _id;
+    private String title;
+    private String singers;
+    private int year;
+    private int stars;
+
+    public Song(String title, String singers, int year, int stars) {
+        this.title = title;
+        this.singers = singers;
+        this.year = year;
+        this.stars = stars;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSingers() {
+        return singers;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+}
