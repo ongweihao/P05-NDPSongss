cbfba827af272a796b517ebd8d75e1d4fbff0208
nickngweipeng <15056201@myrp.edu.sg>
2017-05-18 13:49:42 +0800

    OK

---
+++ app/src/main/java/com/example/nickng/p05_ndpsongs/showSong.java
@@ -0,0 +1,13 @@
+package com.example.nickng.p05_ndpsongs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class showSong extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_song);
+    }
+}
